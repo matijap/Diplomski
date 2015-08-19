f869f981a530c1f4764bb65c3afe595c99d0572d
Matija Parausic <matija.parausic@propulsionapp.com>
2015-08-19 23:34:36 +0200

    galery page

---
+++ ajax/galeryComments.php
@@ -0,0 +1,126 @@
+<div class="one-post-comment">
+    <div class="post-comment-avatar">
+        <img src="img/silhouette.png">
+    </div>
+    <div class="one-post-actions-holder">
+        <a href="javascript:void(0)"><i class="fa fa-heart"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-reply"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-forward"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-warning"></i></a>
+    </div>
+    <div class="one-post-comment-text">
+        <p>
+        This is comment text. This is comment text.  This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text.
+        </p>
+    </div>
+    <div class="post-count-and-date">
+        <p>53 Favs</p>
+        <p>14 Forwards</p>
+        <p class="pull-right">15.06.2015</p>
+    </div>
+</div>
+<div class="one-post-comment">
+    <div class="post-comment-avatar">
+        <img src="img/silhouette.png">
+    </div>
+    <div class="one-post-actions-holder">
+        <a href="javascript:void(0)"><i class="fa fa-heart"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-reply"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-forward"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-warning"></i></a>
+    </div>
+    <div class="one-post-comment-text">
+        <p>
+        This is comment text. This is comment text.  This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text.
+        </p>
+    </div>
+    <div class="post-count-and-date">
+        <p>53 Favs</p>
+        <p>14 Forwards</p>
+        <p class="pull-right">15.06.2015</p>
+    </div>
+</div>
+<div class="one-post-comment">
+    <div class="post-comment-avatar">
+        <img src="img/silhouette.png">
+    </div>
+    <div class="one-post-actions-holder">
+        <a href="javascript:void(0)"><i class="fa fa-heart"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-reply"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-forward"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-warning"></i></a>
+    </div>
+    <div class="one-post-comment-text">
+        <p>
+        This is comment text. This is comment text.  This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text.
+        </p>
+    </div>
+    <div class="post-count-and-date">
+        <p>53 Favs</p>
+        <p>14 Forwards</p>
+        <p class="pull-right">15.06.2015</p>
+    </div>
+</div>
+<div class="one-post-comment">
+    <div class="post-comment-avatar">
+        <img src="img/silhouette.png">
+    </div>
+    <div class="one-post-actions-holder">
+        <a href="javascript:void(0)"><i class="fa fa-heart"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-reply"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-forward"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-warning"></i></a>
+    </div>
+    <div class="one-post-comment-text">
+        <p>
+        This is comment text. This is comment text.  This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text.
+        </p>
+    </div>
+    <div class="post-count-and-date">
+        <p>53 Favs</p>
+        <p>14 Forwards</p>
+        <p class="pull-right">15.06.2015</p>
+    </div>
+</div>
+<div class="one-post-comment">
+    <div class="post-comment-avatar">
+        <img src="img/silhouette.png">
+    </div>
+    <div class="one-post-actions-holder">
+        <a href="javascript:void(0)"><i class="fa fa-heart"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-reply"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-forward"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-warning"></i></a>
+    </div>
+    <div class="one-post-comment-text">
+        <p>
+        This is comment text. This is comment text.  This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text.
+        </p>
+    </div>
+    <div class="post-count-and-date">
+        <p>53 Favs</p>
+        <p>14 Forwards</p>
+        <p class="pull-right">15.06.2015</p>
+    </div>
+</div>
+<div class="one-post-comment">
+    <div class="post-comment-avatar">
+        <img src="img/silhouette.png">
+    </div>
+    <div class="one-post-actions-holder">
+        <a href="javascript:void(0)"><i class="fa fa-heart"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-reply"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-forward"></i></a>
+        <a href="javascript:void(0)"><i class="fa fa-warning"></i></a>
+    </div>
+    <div class="one-post-comment-text">
+        <p>
+        This is comment text. This is comment text.  This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text. This is post text.
+        </p>
+    </div>
+    <div class="post-count-and-date">
+        <p>53 Favs</p>
+        <p>14 Forwards</p>
+        <p class="pull-right">15.06.2015</p>
+    </div>
+</div>
